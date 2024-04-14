ac108ab021916699e7ed36074a1bc917b7786390
ShikshaFinder <janiharsh794@gmail.com>
2024-04-14 12:45:22 +0530

    harsh

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -105,13 +105,10 @@ export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full  left-0 top-0">
       <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
-        Best way to reach out to <br /> Student & Parents
+       LDCE  <br /> 
       </h1>
       <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
-       LDCE is an online platform that helps students and parents to
-        choose the best educational institution for their future. Here we engage
-        the students and parents with diffrent educational institutions and help
-        them by providing them demo lectures of the institutions.
+       LDCE is a college situated in ahmedabad,Gujrat.LDCE is a premier engineering college in gujrat which is known for its quality education and placements.
       </p>
       <br />
       <Link href={"/getstarted"}>
